85e3474e53284f5955115e69aedfdc6c896e8053
siwon <muncool39@gmail.com>
2023-12-21 15:51:45 +0900

    modify(ExecutionTimeInterceptor): add looger

--- src/main/java/com/example/logging/interceptor/ExecutionTimeInterceptor.java
+++ src/main/java/com/example/logging/interceptor/ExecutionTimeInterceptor.java
@@ -3,12 +3,16 @@ package com.example.logging.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class ExecutionTimeInterceptor implements HandlerInterceptor {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     private long startTime;
 
     @Override //handler 메소드 실행 전 호출. execution chain 을 계속 할 지 여부를 return
@@ -26,6 +30,7 @@ public class ExecutionTimeInterceptor implements HandlerInterceptor {
                            ModelAndView modelAndView) throws Exception {
         long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
+        log.info("executionTime : {}", executionTime);
     }
 
     @Override //뷰 렌더링 후 호출. (모든 정리 작업 수행 가능)
